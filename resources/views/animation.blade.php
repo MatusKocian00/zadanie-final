a99b23d96b31046179113f199d7ecfb313e6eb2e
MatusKocian00 <xkocian@stuba.sk>
2022-05-24 12:15:16 +0200

    animated graph

--- resources/views/animation.blade.php
+++ resources/views/animation.blade.php
@@ -2,7 +2,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.3/js/bootstrap-select.min.js" charset="utf-8"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.js" charset="utf-8"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.js"></script>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Animation') }}
--- resources/views/components/graph.blade.php
+++ resources/views/components/graph.blade.php
@@ -1,45 +1,87 @@
 <div class="panel-body">
-    <canvas id="canvas" height="280" width="600"></canvas>
+    <canvas id="canvas" height="280 " width="600"></canvas>
 </div>
 <script>
-    const jsObject = {{Js::from(json_decode($data))}};
+    const jsObject = {
+        {
+            Js::from(json_decode($data))
+        }
+    };
     const labels = []
-    const x1 = []
-    const x2 = []
+    let data = []
+    let data2 = []
     for (let i = 1; i < jsObject.length - 1; i++) {
         labels.push(jsObject[i][1]);
-        x1.push(jsObject[i][2])
-        x2.push(jsObject[i][3])
-    }
-    const data = {
-        labels: labels,
-        datasets: [{
-                label: 'x1',
-                data: x1,
-                fill: false,
-                borderColor: 'green',
-                tension: 0.5
-            },
-            {
-                label: 'x2',
-                data: x2,
-                fill: false,
-                borderColor: 'red',
-                tension: 0.1
-            }
-        ]
+        data.push({
+            x: jsObject[i][1],
+            y: jsObject[i][2]
+        })
+        data2.push({
+            x: jsObject[i][1],
+            y: jsObject[i][3]
+        })
+
     }
     var ctx = document.getElementById("canvas").getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: data,
-        options: {
-            elements: {
-                point: {
-                    radius: 0
+    const totalDuration = 5000;
+    const delayBetweenPoints = totalDuration / data.length;
+    const previousY = (ctx) => ctx.index === 0 ? ctx.chart.scales.y.getPixelForValue(100) : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1].getProps(['y'], true).y;
+    const animation = {
+        x: {
+            type: 'number',
+            easing: 'linear',
+            duration: delayBetweenPoints,
+            from: NaN, // the point is initially skipped
+            delay(ctx) {
+                if (ctx.type !== 'data' || ctx.xStarted) {
+                    return 0;
                 }
+                ctx.xStarted = true;
+                return ctx.index * delayBetweenPoints;
+            }
+        },
+        y: {
+            type: 'number',
+            easing: 'linear',
+            duration: delayBetweenPoints,
+            from: previousY,
+            delay(ctx) {
+                if (ctx.type !== 'data' || ctx.yStarted) {
+                    return 0;
+                }
+                ctx.yStarted = true;
+                return ctx.index * delayBetweenPoints;
             }
         }
-        
-    });
+    };
+    const config = {
+        type: 'line',
+        data: {
+            datasets: [{
+                    borderColor: 'red',
+                    borderWidth: 1,
+                    radius: 0,
+                    data: data,
+                },
+                {
+                    borderColor: 'blue',
+                    borderWidth: 1,
+                    radius: 0,
+                    data: data2,
+                }
+            ]
+        },
+        options: {
+            animation,
+            plugins: {
+                legend: true
+            },
+            scales: {
+                x: {
+                    type: 'linear'
+                },
+            }
+        }
+    };
+    var myChart = new Chart(ctx, config);
 </script>
