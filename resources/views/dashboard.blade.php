49a5b5b51321aad0ea7f6aaa8fbbdf0bddf26634
Dominika Bemberakova <dominika.bemberakova@gmail.com>
2022-05-29 13:03:11 +0200

    dashboard fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,8 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200 text-center">
+                    @if (session('message'))
+                    <div class="alert alert-success">
+                        {{ session('message') }}
+                    </div>
+                    @else
                     {{__('messages.welcome')}}
-                    <br>
+                    @endif
                 </div>
             </div>
         </div>
